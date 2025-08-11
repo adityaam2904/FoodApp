b4204a6eaaa7effd64b6c8d29173c7bae5bec723
Aditya Amrutkar <aditya.am2904@gmail.com>
2025-08-11 19:58:08 +0530

    Add files via upload

---
+++ Admin/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
+import { Routes,Route } from 'react-router-dom'
+import Addproduct from './Pages/Addproduct/Addproduct'
+import List from './Pages/List/List'
+import Orders from './Pages/Orders/Orders'
+ import { ToastContainer } from 'react-toastify';
+ import 'react-toastify/dist/ReactToastify.css';
+
+
+const App = () => {
+  const url="http://localhost:4000";
+  return (
+    <div>
+      <ToastContainer/>
+      <Navbar></Navbar>
+      <hr></hr>
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Addproduct url={url}/>}/>
+          <Route path="/list" element={<List url={url}/>}/>
+          <Route path="/order " element={<Orders url={url}/>}/>
+        </Routes>
+      </div>
+    </div> 
+  )
+}
+
+export default App
---
+++ Admin/src/Pages/Orders/Orders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Orders.css"
+const Orders = ({url}) => {
+  return (
+    <div></div>
+  )
+}
+
+export default Orders
